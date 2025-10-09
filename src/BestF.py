e953345983562978f50808ec47f43ac0dd539a8e
a-nosnitram <stasymartinson@gmail.com>
2025-10-09 19:16:02 +0100

    BEST F

---
+++ src/BestF.py
@@ -0,0 +1,71 @@
+# A* Search Algorithm
+# starts with an initial node,
+# explores based on heuristic
+# h(n) = path length
+
+from queue import PriorityQueue
+from node import Node
+from state_space import StateSpace
+from coord import Coord
+import itertools
+from digitiser import DrawLine
+
+
+class BestF():
+    def __init__(self, size, start, goal, obs, verbose):
+        self.state_space = StateSpace(size, start, goal, obs)
+        self.verbose = verbose
+        self.frontier = PriorityQueue()
+        self._counter = itertools.count()
+
+    def heuristic(self, coord):
+        # euclidean distance
+        return ((coord.x - self.state_space.goal.x) ** 2 + (coord.y - self.state_space.goal.y) ** 2) ** 0.5
+
+    # search through state space, where you're only allowed to move
+    # along obstacle edges (not through obstacles)
+    def search(self):
+        start_node = Node(self.state_space.start)
+        start_node.cost = 0
+        start_node.her = self.heuristic(self.state_space.start)
+        start_node.total_cost = start_node.her
+
+        self.frontier.put(
+            (start_node.total_cost, next(self._counter), start_node))
+
+        explored = set()
+        step = 0
+        while not self.frontier.empty():
+            if self.verbose:
+                print(str([str(n.coord) + "{:.1f}".format(n.total_cost)
+                      for _, _, n in self.frontier.queue]).replace("'", ""))
+
+            # queue items are (total_cost, counter, node)
+            current_node = self.frontier.get()[2]
+            explored.add((current_node.coord.x, current_node.coord.y))
+            if current_node.coord == self.state_space.goal:
+                # 1.0 cost between adjacent coordinates and √2 for diagonals
+                if self.verbose:
+                    print(len(explored))
+                    print("".join([str(coord)
+                          for coord in explored]).replace(" ", ""))
+                return float(current_node.cost)
+            # get neighbors
+            neighbors = self.state_space.get_neighbors(current_node)
+            for neighbor in neighbors:
+                neighbor_node = Node(neighbor, current_node)
+                # Calculate cost: 1.0 for adjacent, √2 for diagonal
+                line = DrawLine(current_node.coord, neighbor)
+                distance = line.length
+                neighbor_node.cost = current_node.cost + distance
+                neighbor_node.her = self.heuristic(neighbor)
+                neighbor_node.total_cost = neighbor_node.her
+                coord_key = (neighbor_node.coord.x, neighbor_node.coord.y)
+                if coord_key not in explored:
+                    # push (cost, tie-breaker, node) so heap never compares Node
+                    self.frontier.put(
+                        (neighbor_node.total_cost, next(self._counter), neighbor_node))
+            step += 1
+        if self.verbose:
+            print(len(explored))
+        return None  # no path found :((((
